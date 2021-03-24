5af7a5cf6dd12df62b50033208c78ad6bb38cb9f
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-24 17:11:56 +0530

    Plant

---
+++ Mario/Assets/Script/Enemy/Piranha/PiranhaMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiranhaMovement : MonoBehaviour
+{
+    public float verticalSpeed;
+    public Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.transform.position += new Vector3(0, verticalSpeed*Time.deltaTime, 0);
+    }
+}
