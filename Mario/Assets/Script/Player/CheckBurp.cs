6ab05b38f4e5e17498acdb5b117bb1af537c347b
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-27 11:50:12 +0530

    BurpAudioAdded

---
+++ Mario/Assets/Script/Player/CheckBurp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBurp : MonoBehaviour
+{
+    public AudioSource burpAudio;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "MystryTiles" || other.tag == "WoodenTiles" || other.tag == "hiddenScoreTile")
+        {
+            burpAudio.Play();
+        }
+    }
+}
