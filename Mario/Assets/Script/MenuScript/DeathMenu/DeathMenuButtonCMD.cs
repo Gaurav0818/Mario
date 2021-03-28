33aa2d76e5615d832215c7c893053252e6d238d9
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-28 19:29:53 +0530

    Setting menu

---
+++ Mario/Assets/Script/MenuScript/DeathMenu/DeathMenuButtonCMD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenuButtonCMD : MonoBehaviour
+{
+    public void OnRestartButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnMenuButtonPress()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Mario/Assets/Script/MenuScript/MainMenu/MainMenuButtonCMD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonCMD : MonoBehaviour
+{
+    public void OnPlayButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnExitButtonPress()
+    {
+        Application.Quit();
+    }
+}
