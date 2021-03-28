333a9cb47b439fda34a33b6cc1b235f81213d127
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-29 00:45:04 +0530

    pause menu (incomplete)

---
+++ Mario/Assets/Script/PauseGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool pauseGame = false;
+    public GameObject pauseOption;
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Escape) && pauseGame == false)
+        {
+            pauseGame = true;
+        }
+        if (pauseGame == true)
+        {
+            Time.timeScale = 0;
+            pauseOption.SetActive(true);
+            
+        }
+        if (Input.GetKey(KeyCode.Escape) && pauseGame == true)
+        {
+            pauseGame = false;
+            Time.timeScale = 1;
+        }
+
+    }
+}
