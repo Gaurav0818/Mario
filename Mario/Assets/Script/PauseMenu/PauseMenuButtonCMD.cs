d2ce40114cdd03f563d85e56d2a37f0f342b3b01
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-29 14:31:45 +0530

    OptionMenu

--- Mario/Assets/Script/PauseMenu/PauseMenuButtonCMD.cs
+++ Mario/Assets/Script/PauseMenu/PauseMenuButtonCMD.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuButtonCMD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnRestartButtonPress()
     {
-        
+        Scene scene = SceneManager.GetActiveScene(); 
+        SceneManager.LoadScene(scene.name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnOptionButtonPress()
     {
-        
+        FindObjectOfType<ifControlPressed>().option = true;
+    }
+    public void OnMenuButtonPress()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ Mario/Assets/Script/PauseMenu/controlMenu/ifControlPressed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifControlPressed : MonoBehaviour
+{
+    public bool option = false;
+    public GameObject optionMenu;
+    public GameObject escapeMenu;
+
+    private void Update()
+    {
+        if (option == true)
+        {
+            escapeMenu.SetActive(false);
+            optionMenu.SetActive(true);
+        }
+    }
+    public void backPressed()
+    {
+        optionMenu.SetActive(false);
+        escapeMenu.SetActive(true);
+    }
+}
