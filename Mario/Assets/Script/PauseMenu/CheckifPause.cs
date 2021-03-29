4796701610451ec7433eb6c4c92a905551d408ea
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-03-29 13:40:26 +0530

    pauseMenu

---
+++ Mario/Assets/Script/PauseMenu/CheckifPause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckifPause : MonoBehaviour
+{
+    public bool gamepause = false;
+    public float timer;
+    private void Update()
+    {
+        if (gamepause == false)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Debug.Log("Escape pressed (toEnter)");
+                gamepause = true;
+                
+            }
+        }
+
+        if (gamepause == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                Debug.Log("Escape pressed (toClose)");
+                gamepause = false;
+            }
+        }
+    }
+}
---
+++ Mario/Assets/Script/PauseMenu/ifPause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifPause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private void Update()
+    {
+        if (FindObjectOfType<CheckifPause>().gamepause == true)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+
+        if(FindObjectOfType<CheckifPause>().gamepause == false)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+    }
+}
